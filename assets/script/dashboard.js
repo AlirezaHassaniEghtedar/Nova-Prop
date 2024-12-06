7e30d230d66077cf17c9499e8ad3d89f3153cef9
Alireza Hassani Eghtedar <alirezaprogrammer2001@gmail.com>
2024-12-07 01:41:21 +0330

    completing dashbaord

---
+++ assets/script/dashboard.js
@@ -0,0 +1,7 @@
+// const dashboardMenuIcon = document.querySelector('.sidebar-toggle-btn img');
+
+// dashboardMenuIcon.addEventListener('click' , handleDashboardMenuIconClick)
+
+// function handleDashboardMenuIconClick(e) {
+
+// }
